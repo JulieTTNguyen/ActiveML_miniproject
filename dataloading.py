f8c9620869772ef190f85f65231bfe6628404124
Julie Thanh Thanh Nguyen <julienguyen454@gmail.com>
2026-03-17 14:17:22 +0100

    dataloading

---
+++ dataloading.py
@@ -0,0 +1,42 @@
+from sklearn.model_selection import StratifiedShuffleSplit
+
+def prepare_data(cov_type, n_init, seed=None):
+    """Extract classes and features and split data in training, pool, and test
+    sets.
+
+    PARAMETERS
+    ----------
+    cov_type
+        The cov_type data object.
+    n_init : int
+        Number of initial data points in the training set.
+    seed : int
+        Seed for reproducibility.
+    """
+    X = cov_type['data'][:1000]
+    y = cov_type['target'][:1000]
+
+    n = len(X)
+    assert n_init <= n
+
+    # Split in train, pool, and test set
+    # Use stratified split to make sure we sample all classes equally
+    sss = StratifiedShuffleSplit(n_splits=1, train_size= n_init / n, random_state=seed)
+    train, pool = next(sss.split(X, y))
+    sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=seed)
+    pool_, test = next(sss.split(X[pool], y[pool]))
+
+    return dict(
+        train = dict(
+            X=X[train],
+            y=y[train]
+        ),
+        pool = dict(
+            X=X[pool[pool_]],
+            y=y[pool[pool_]]
+        ),
+        test = dict(
+            X=X[pool[test]],
+            y=y[pool[test]]
+        )
+    )
