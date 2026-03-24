4608f0918b32c181584b5512b0d346e360cb8f3c
KatinkaMG <s235058@dtu.dk>
2026-03-24 16:05:00 +0100

    Accuracy by class

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def uncertainty(label_probs,n_points,measure):
-    if measure=='least confident':
+    if measure=='least_confident':
         return np.argsort([1-max(y) for y in label_probs])[-n_points:]
     if measure=='margin':
         return np.argsort([np.sort(y)[-1]-np.sort(y)[-2] for y in label_probs])[:n_points]
@@ -9,7 +9,7 @@ def uncertainty(label_probs,n_points,measure):
         return np.argsort([-sum(y*np.log(y)) for y in label_probs])[-n_points:]
 
 
-def train_iteratively(data, model, measure, addn = 1):
+def train_iteratively(data, model, measure, addn = 1, iterations = 25):
     Xtrain = data["train"]["X"]
     ytrain = data["train"]["y"]
     ninit = Xtrain.shape[0]
@@ -26,17 +26,24 @@ def train_iteratively(data, model, measure, addn = 1):
     poolidx=np.arange(len(Xpool),dtype=int)
 
     testacc = []
+    testacc_byclass = [[] for i in range(7)]
+
     data = Xtrain
     labels = ytrain
 
-    for i in range(25):
+    for i in range(iterations):
         model.fit(data, labels);
         #predict and calculate the accuracy
         ypred = model.predict(Xtest)
 
         #calculate accuracy on test set
+        for j,clas in enumerate(sorted(set(ytest))):
+            class_idx = ytest == clas
+            testacc_byclass[j].append((ninit+i*addn, sum(ypred[class_idx]==clas)/sum(class_idx)))
+        
         accuracy = sum(ytest == ypred)/len(ytest)
         testacc.append((ninit+i*addn,accuracy)) #add in the accuracy
+        
 
         #find next index from pool (with highest uncertainty)
         pool = np.take(Xpool,poolidx,axis=0)
@@ -55,4 +62,4 @@ def train_iteratively(data, model, measure, addn = 1):
         data = np.vstack((data, np.take(Xpool, top_indices, axis=0)))
         labels = np.concatenate((labels, np.take(ypool, top_indices, axis=0)))
 
-    return testacc
+    return testacc, testacc_byclass
